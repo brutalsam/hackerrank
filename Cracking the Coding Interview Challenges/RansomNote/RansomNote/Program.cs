5e51b95b5172ff4587031dbb25687f94273afc84
Semen Gzhesiak <brutalsam@gmail.com>
2017-03-14 12:19:17 +0200

    Added Ransome note solution

---
+++ Cracking the Coding Interview Challenges/RansomNote/RansomNote/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace RansomNote
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] tokens_m = Console.ReadLine().Split(' ');
+            int m = Convert.ToInt32(tokens_m[0]);
+            int n = Convert.ToInt32(tokens_m[1]);
+            string[] magazine = Console.ReadLine().Split(' ');
+            string[] ransom = Console.ReadLine().Split(' ');
+
+            Console.WriteLine(CanMakeRansom(magazine, ransom) ? "Yes" : "No");
+            Console.ReadLine();
+        }
+
+        private static bool CanMakeRansom(string[] magazine, string[] ransom)
+        {
+            var magazineWords = magazine.GroupBy(x => x).ToDictionary(x => x.Key, y => y.Count());
+
+            var ransomWords = ransom.GroupBy(x => x).ToDictionary(x => x.Key, y => y.Count());
+            foreach (var word in ransomWords)
+            {
+                if (!magazineWords.ContainsKey(word.Key))
+                    return false;
+                if (magazineWords[word.Key] < word.Value)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
