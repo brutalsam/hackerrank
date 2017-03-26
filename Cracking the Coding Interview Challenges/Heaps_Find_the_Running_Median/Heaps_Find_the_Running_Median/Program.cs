80bb2753d99bec78d0fcd59b728d7c9dc65eb346
Semen Gzhesiak <Semen Gzhesiak>
2017-03-26 12:15:57 +0300

    Heaps_Find_the_Running_Median

---
+++ Cracking the Coding Interview Challenges/Heaps_Find_the_Running_Median/Heaps_Find_the_Running_Median/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heaps_Find_the_Running_Median
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{ 
+			int n = Convert.ToInt32(Console.ReadLine());
+			int[] a = new int[n];
+			for(int a_i = 0; a_i<n; a_i++){
+			   a[a_i] = Convert.ToInt32(Console.ReadLine());   
+        }
+}
+	}
+}
